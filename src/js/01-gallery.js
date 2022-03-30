997bccedec95f70341a0fa6f02c0279e10cce8c2
Anton K <89594975+Pusttak@users.noreply.github.com>
2022-03-31 03:59:59 +0400

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+makeGalleryMarkup(galleryItems);
+
+function makeGalleryMarkup(galleryItems) {
+    const galleryMarkup = galleryItems.map(({ preview, original, description }) => {
+        const item = document.createElement('li');
+
+        const link = document.createElement('a');
+        link.classList.add('gallery__item');
+        link.href = original;
+
+        const img = document.createElement('img');
+        img.classList.add('gallery__image');
+        img.src = preview;
+        img.alt = description;
+
+        item.appendChild(link);
+        link.appendChild(img);
+
+        return item;
+    });
+
+    galleryRef.append(...galleryMarkup);
+
+    let lightbox = new SimpleLightbox('.gallery a', {
+        captions: true,
+        captionPosition: 'bottom',
+        captionsData: 'alt',
+        captionDelay: 250,
+        showCounter: false,
+    });
+}
