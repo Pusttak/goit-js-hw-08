bcbf9cc047f71dfddb7242e84bebdc64fe28977c
Anton K <89594975+Pusttak@users.noreply.github.com>
2022-04-01 05:58:40 +0400

    finish

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+const formRef = document.querySelector('.feedback-form');
 
+formRef.addEventListener('input', onTextFormInput);
+formRef.addEventListener('click', onInputSubmit);
+
+let formData = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+
+populateFormData();
+
+function onTextFormInput(evn) {
+  formData[evn.target.name] = evn.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function onInputSubmit(evn) {
+  evn.preventDefault();
+
+  if (evn.target.nodeName === 'BUTTON') {
+    const { email, message } = evn.currentTarget.elements;
+
+    console.log(email.value, message.value);
+    evn.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state');
+    formData = {};
+  }
+}
+
+function populateFormData() {
+  const { email, message } = formData;
+
+  if (email || message) {
+    formRef.elements.email.value = email || null;
+    formRef.elements.message.value = message || null;
+  }
+}
